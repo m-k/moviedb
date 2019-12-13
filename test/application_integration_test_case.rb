a088a6b5b9e98d2dc98eb0fea5ed80591320f591
vlaew <vlaew5@gmail.com>
2019-12-13 18:29:18 +0300

    #9: Add integration tests for user sign up/in

--- test/application_integration_test_case.rb
+++ test/application_integration_test_case.rb
@@ -3,14 +3,14 @@
 require 'test_helper'
 
 class ApplicationIntegrationTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
   Capybara.server = :puma, { Silent: true }
 
   def sign_in(user)
-    visit '/'
+    visit '/users/sign_in'
 
     fill_in 'user[email]', with: users(user).email
-    fill_in 'user[password]', with: 'welcome'
+    fill_in 'user[password]', with: 'password'
     click_on 'Log in'
   end
 end
