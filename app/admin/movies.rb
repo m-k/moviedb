4a1fc1d433bb29c518261c86d66b52e5923e208e
vlaew <vlaew5@gmail.com>
2019-12-14 21:28:03 +0300

    #10: Add show/edit for movies

--- app/admin/movies.rb
+++ app/admin/movies.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register Movie do
-  permit_params :title, :slug, :plot, :poster, :rating
+  permit_params :title, :slug, :plot, :poster, :rating, genre_ids: []
   includes :genres
 
   controller do
@@ -19,5 +19,32 @@ ActiveAdmin.register Movie do
     column :rating
     column :created_at
     actions
+  end
+
+  show do
+    attributes_table do
+      row :title
+      row :slug
+      row(:genres) { |movie| movie.genres.map(&:name) }
+      row :plot
+      row :poster
+      row :rating
+      row :created_at
+    end
+  end
+
+  form do |f|
+    f.semantic_errors
+
+    f.inputs do
+      f.input :title
+      f.input :slug
+      f.input :plot
+      f.input :genres, as: :check_boxes, multiple: true, collection: Genre.order(name: :asc)
+      f.input :poster
+      f.input :rating
+    end
+
+    f.actions
   end
 end
